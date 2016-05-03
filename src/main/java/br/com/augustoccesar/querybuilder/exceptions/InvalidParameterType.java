fbdf2bbad1d6f83f091e7fef56a43e3b01e87115
augustoccesar <augusto.acfs@gmail.com>
2016-05-03 14:28:24 -0300

    QueryExecutable first try.

---
+++ src/main/java/br/com/augustoccesar/querybuilder/exceptions/InvalidParameterType.java
@@ -0,0 +1,10 @@
+package br.com.augustoccesar.querybuilder.exceptions;
+
+/**
+ * Created by augustoccesar on 5/3/16.
+ */
+public class InvalidParameterType extends Exception {
+    public InvalidParameterType() {
+        super("Invalid parameter type.");
+    }
+}
