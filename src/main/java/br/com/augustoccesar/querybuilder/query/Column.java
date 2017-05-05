e04f1f3ebffaf9607c5cde7b35de1bb129db84d6
Augusto César Freitas e Silva <augusto.acfs@gmail.com>
2017-05-05 12:00:21 -0300

    Added possibility to create columns using markdown

--- src/main/java/br/com/augustoccesar/querybuilder/query/Column.java
+++ src/main/java/br/com/augustoccesar/querybuilder/query/Column.java
@@ -1,5 +1,8 @@
 package br.com.augustoccesar.querybuilder.query;
 
+import br.com.augustoccesar.querybuilder.exceptions.InvalidPattern;
+
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -8,35 +11,72 @@ import java.util.regex.Pattern;
 public class Column {
     private String prefix;
     private String name;
-    private String customAlias;
+    private String alias;
     private boolean distinct = false;
 
     // Constructors
 
-    public Column(String name) {
+    public Column(String name, boolean distinct) {
         this.name = name;
+        this.distinct = distinct;
     }
 
-    public Column(String prefix, String name) {
+    public Column(String prefix, String name, boolean distinct) {
         this.prefix = prefix;
         this.name = name;
+        this.alias = prefix + "_" + name;
+        this.distinct = distinct;
     }
 
-    public Column(String prefix, String name, String customAlias) {
+    public Column(String prefix, String name, String customAlias, boolean distinct) {
         this.name = name;
         this.prefix = prefix;
-        this.customAlias = customAlias;
+        this.alias = customAlias;
+        this.distinct = distinct;
     }
 
     // Builder
 
-    public Column fromMarkdown(String columnMarkdown) {
-        Pattern markdownWithTableAlias = Pattern.compile("\\{(\\w+)\\}(\\w+)");
-        Pattern markdownWithCustomAlias = Pattern.compile("(\\w+)\\{(\\w+)\\}");
-        Pattern fullMarkdown = Pattern.compile("\\{(\\w+)\\}\\w+\\{(\\w+)\\}");
+    public static Column fromMarkdown(String columnMarkdown) {
+        Pattern markdownWithTableAlias = Pattern.compile("\\{(\\w+)\\}(\\**\\w+)");
+        Pattern fullMarkdown = Pattern.compile("\\{(\\w+)\\}(\\**\\w+)\\{(\\w+)\\}");
 
-//        return new Column();
-        return null;
+        Matcher markdownWithTableAliasMatcher = markdownWithTableAlias.matcher(columnMarkdown);
+        Matcher fullMarkdownMatcher = fullMarkdown.matcher(columnMarkdown);
+
+        if(markdownWithTableAliasMatcher.matches()){
+            String prefix = markdownWithTableAliasMatcher.group(1);
+            String name = markdownWithTableAliasMatcher.group(2);
+            boolean distinct = false;
+
+            if (name.contains("*")){
+                name = name.replace("*", "");
+                distinct = true;
+            }
+
+            if(prefix.equals("_")){
+                return new Column(name, distinct);
+            }else {
+                return new Column(prefix, name, distinct);
+            }
+        }else if(fullMarkdownMatcher.matches()){
+            String prefix = fullMarkdownMatcher.group(1);
+            String name = fullMarkdownMatcher.group(2);
+            String customAlias = fullMarkdownMatcher.group(3);
+            boolean distinct = false;
+
+            prefix = prefix.equals("_") ? null : prefix;
+            customAlias = customAlias.equals("_") ? null : customAlias;
+
+            if (name.contains("*")){
+                name = name.replace("*", "");
+                distinct = true;
+            }
+
+            return new Column(prefix, name, customAlias, distinct);
+        }else{
+            throw new InvalidPattern("Column");
+        }
     }
 
     // Getters and Setters
@@ -49,8 +89,8 @@ public class Column {
         return prefix;
     }
 
-    public String getCustomAlias() {
-        return customAlias;
+    public String getAlias() {
+        return alias;
     }
 
     public boolean isDistinct() {
